5d94f1eca827732b6e67df5c6bc20e9fadc68d6c
SabujPaul <sabujpaul05@gmail.com>
2024-01-07 14:22:43 +0600

    configure postgresql and creating primary model

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    due_date = models.DateField()
+    img = models.ImageField(upload_to = 'pics')
+    priority = models.CharField(max_length=20, choices=[('low', 'Low'), ('medium', 'Medium'), ('high', 'High')])
+    is_complete = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
